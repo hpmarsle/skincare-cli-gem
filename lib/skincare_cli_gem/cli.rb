2021b434f7e9b5a1dac2d9fdb9bf56fedbaf5b5c
Maria Le <hp.mars.le@gmail.com>
2019-10-10 21:35:13 -0500

    Outline CLI

--- lib/skincare_cli_gem/cli.rb
+++ lib/skincare_cli_gem/cli.rb
@@ -4,8 +4,8 @@ class SkincareCliGem::CLI
         puts "Welcome to your skincare curations."
         puts "-"*40
         list_skin_concerns
-        puts "Please select the corresponding number to your skin concern:"
-        
+        menu
+        goodbye
     end
 
     def list_skin_concerns
@@ -17,5 +17,30 @@ class SkincareCliGem::CLI
             4. Sensitive Skin
         DOC
     end
+
+    def menu 
+        
+        input = nil
+        while input != "exit"
+            puts "Please type the corresponding number to your skin concern, type list for the options, or type exit to leave:"
+            input = gets.strip.downcase
+            case input 
+            when "1"
+                puts "info on acne"
+            when "2"
+                puts "info on antiaging"
+            when "3"
+                puts "info on dryness"
+            when "4"
+                puts "info on sensitive skin"
+            when "list"
+                list_skin_concerns
+            end
+        end
+    end 
+
+    def goodbye
+        puts "Thanks for visiting! See you next time!"
+    end 
 
 end
