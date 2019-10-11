3e5a75f6f2173b0dc4183c82aec7693d858ea286
Maria Le <hp.mars.le@gmail.com>
2019-10-10 22:12:39 -0500

    Edit SkinConcern object

--- lib/skincare_cli_gem/concern.rb
+++ lib/skincare_cli_gem/concern.rb
@@ -3,5 +3,11 @@ class SkincareCliGem::SkinConcern
 
     def self.all
        #should return all skincare conccerns 
+       puts <<~DOC 
+            1. Acne
+            2. Anti-aging
+            3. Dryness
+            4. Sensitive Skin
+        DOC
     end
 end
