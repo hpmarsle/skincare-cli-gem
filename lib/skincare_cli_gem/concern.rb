7362a9b06481aa5fc05067646300ca5ec4dec9eb
Maria Le <hp.mars.le@gmail.com>
2019-10-13 16:27:01 -0500

    Start adding products feature

--- lib/skincare_cli_gem/concern.rb
+++ lib/skincare_cli_gem/concern.rb
@@ -1,18 +1,21 @@
 # require "pry"
 class SkinConcern
-    attr_accessor :name, :summary#, :products, :url
+    attr_accessor :name, :summary, :products
     @@all = []
 
     def initialize(name)
         @name = name
         @@all << self 
-    #     # @products = []
+        @products = []
     end
 
     def self.all
         @@all
     end
     
+    def list_products
+        
+    end 
     # def add_product(product)
     #   if !product.is_a?(Product)
     #     raise InvalidType, "must be an Article"
